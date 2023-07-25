1b23a7877a6b1fcb0e789a9538971ed13c135a09
minhnghialk <nghia13031995@gmail.com>
2023-07-25 14:30:06 +0700

    connect MySQL

---
+++ src/databases/mySQL.js
@@ -0,0 +1,31 @@
+// Kích hoạt env
+import dotenv from "dotenv";
+dotenv.config();
+
+import mysql from "mysql";
+let mySQL = mysql.createConnection({
+  host: process.env.MY_SQL_HOST,
+  user: process.env.MY_SQL_USER_NAME,
+  password: process.env.MY_SQL_PASSWORD,
+  database: process.env.MY_SQL_DATABASE_NAME,
+});
+
+function mysqlConnect() {
+  try {
+    mySQL.connect((err) => {
+      if (err) {
+        console.log("Lỗi về MySQL:", err.sqlMessage);
+      } else {
+        console.log("Kết nối MySQL thành công");
+      }
+    });
+  } catch (err) {
+    console.log("Lỗi cú pháp");
+  }
+}
+// mysqlConnect();
+
+module.exports = {
+  mysqlConnect,
+  //   mySQL,
+};
--- src/server.js
+++ src/server.js
@@ -6,6 +6,18 @@ dotenv.config();
 import express from "express";
 const server = express();
 
+// Connect MySQL
+// import mySQL from "./databases/mySQL";
+import { mysqlConnect } from "./databases/mySQL";
+mysqlConnect();
+
+// connection.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+//   if (error) throw error;
+//   console.log("The solution is: ", results[0].solution);
+// });
+
+// connection.end();
+
 server.listen(process.env.SERVER_PORT, () => {
   console.log(
     `Server đã chạy tại: ${process.env.SERVER_HOST}:${process.env.SERVER_PORT}/`
